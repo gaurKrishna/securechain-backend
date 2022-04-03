813e98aeeb485af867987e80e450f89d03138742
Krishna Gaur <gaurkrishna498@gmail.com>
2022-04-04 00:30:41 +0530

    added url for optimization

--- optimization/urls.py
+++ optimization/urls.py
@@ -4,5 +4,5 @@ from rest_framework import urlpatterns
 from .views import OptimizationApi
 
 urlpatterns = [
-    path("optimization/<int:supplychain>/<int:entity>", OptimizationApi.as_view(), name="optimizationapi"),
+    path("optimization/<int:supplychain>/<int:entity>/", OptimizationApi.as_view(), name="optimizationapi"),
 ]
